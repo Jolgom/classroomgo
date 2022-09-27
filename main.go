42e685add9d864d0d70928e6ed9149f68e4f5b3a
jolgom <jolgom@gmail.com>
2022-09-26 20:50:44 -0500

    CICLOS: SOLO EXISTE FOR

--- main.go
+++ main.go
@@ -1,53 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-// Redundar en codigo -FUNCIONES Y FUNCIONES ANONIMAS
-
-// 1. se utiliza la palabra reservada "func"
-func nombreFuncion(nombreVariable string) {
-	fmt.Println(nombreVariable)
-}
-
-// 2. en ocaciones se necesitan multiples argumentos y tipos de datos
-// Se puede agrupár los mismos tipos de datos ejemplo: a, b int
-func multipleArgumento(a, b int, c float64, d string, e bool) {
-	fmt.Println(a, b, c, d, e)
-}
-
-// 3. Funcion que retorna un valor.
-func retornaValor(base, altura uint) uint {
-	return base * altura
-}
-
-// 4. Funcion que retoran varios valores
-func retoroMultiple(radio float64) (circunferenciaCirculo, areaCirculo float64) {
-	return 2 * math.Pi * radio, math.Pi * math.Pow(radio, 2)
-
-}
+// *** CICLOS
+// EN go SOLO EXISTE UN CICLO, << for >>
 
 func main() {
-	// se invoca la funcion llamandola por su nombre y escribiendo en los parentesis el valor de los argumentos o variables
+	// 01.	for basico: condicional (FOR TRADICIONAL)
+	// 		IDNICAR AL INICIO PARA QUE ITERE, INCREMENTE Y FINALICE SEGUN LA CONDICION.
+	for i := 0; i <= 3; i++ {
+		fmt.Println("la iternacia es: ", i)
+	}
 
-	// 1. funcion nombreFuncion
-	nombreFuncion("Hola Mundo")
+	// 02.	for while
+	// 		SE DEBE CUMPLIR UNA CONDICION PARA QUE FINALICE EL CICLO.
+	//		la condicion se define previamente
+	contador := 4
+	for contador < 8 {
+		fmt.Println("la iternacia es: ", contador)
+		contador++
+	}
 
-	// 2. funcion multipleArgumneto
-	multipleArgumento(1, 2, 3.1415, "hola", false)
-
-	// 3. funcion retornaValor
-	areaRectangulo := retornaValor(2, 4)
-	fmt.Println("Area Rectangulo: ", areaRectangulo)
-
-	// 4. funcion retornoMultiple
-	circunferencia, areaCirculo := retoroMultiple(2)
-	fmt.Printf("Circunferencia: %g \nArea de un circulo: %g\n", circunferencia, areaCirculo)
-
-	// 4.1 funcion retornoMultiple, CON UN SOLO RETURN
-	_, areaCirculo2 := retoroMultiple(3)
-	// SE REEMPLAZA LA VARIABLE A RETORNAR POR UN guion piso "_"
-	fmt.Printf("Area de un circulo: %g\n", areaCirculo2)
+	// 04. for REVERSA
+	contador = 11
+	for 8 <= contador {
+		fmt.Println("la iternacia es: ", contador)
+		contador--
+	}
 }
